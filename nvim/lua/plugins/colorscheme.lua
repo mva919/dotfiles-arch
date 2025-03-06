fde861bd016d4ceead4bf97c7fa1f36553e27991
Marcos <mva919@gmail.com>
2025-03-05 22:52:02 -0800

    Fix background being a lighter color in floating windows

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
-return { 
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function() 
-	require("gruvbox").setup({
-	  contrast="hard"
-	})
-	vim.opt.background = "dark"
-	vim.cmd("colorscheme gruvbox")
-    end 
-} 
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = function()
+    require("gruvbox").setup({
+      contrast = "hard",
+      transparent_mode = true,
+    })
+    vim.opt.background = "dark"
+    vim.cmd("colorscheme gruvbox")
+  end,
+}
